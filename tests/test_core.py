6a12dad9b5a4de7bb3bbbaf110ea2e86c1d23a78
Olexandr Samokhval <dlord2423@gmail.com>
2026-04-15 04:54:29 +0300

    add CI, test

---
+++ tests/test_core.py
@@ -0,0 +1,17 @@
+import unittest
+from core import calculate_average
+
+class TestCoreFunctions(unittest.TestCase):
+    
+    def test_calculate_average(self):
+        scores = [90, 80, 70, 60]
+        expected_average = 75.0
+        self.assertEqual(calculate_average(scores), expected_average)
+
+    def test_calculate_average_empty(self):
+        scores = []
+        expected_average = 0
+        self.assertEqual(calculate_average(scores), expected_average)
+
+if __name__ == "__main__":
+    unittest.main()
