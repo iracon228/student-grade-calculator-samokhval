868b5928b3b71781f853965f3ec00e5b18740082
Olexandr Samokhval <dlord2423@gmail.com>
2026-04-15 05:23:31 +0300

    fix lint issues

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,10 @@
 import unittest
+
 from core import calculate_average
 
+
 class TestCoreFunctions(unittest.TestCase):
-    
+
     def test_calculate_average(self):
         scores = [90, 80, 70, 60]
         expected_average = 75.0
@@ -12,6 +14,7 @@ class TestCoreFunctions(unittest.TestCase):
         scores = []
         expected_average = 0
         self.assertEqual(calculate_average(scores), expected_average)
+
 
 if __name__ == "__main__":
     unittest.main()
