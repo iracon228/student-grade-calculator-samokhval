8ee884928a2d0e6f871a7cc265bcec4daa95290f
Olexandr Samokhval <dlord2423@gmail.com>
2026-04-15 05:14:02 +0300

    break average calculation for CI demo

--- core.py
+++ core.py
@@ -9,7 +9,7 @@ GRADE_THRESHOLDS = [
 def calculate_average(scores):
     if not scores:
         return 0
-    return round(sum(scores) / len(scores), 2)
+    return round(sum(scores) / (len(scores) + 1), 2)
 
 def get_grade(average):
     for threshold, grade in GRADE_THRESHOLDS:
